9033a87ac6fb531e23125cdea10efd9a5b16a512
dfath <fathrico@gmail.com>
2016-12-16 08:55:42 +0700

    front controller

--- framework/index.php
+++ framework/index.php
@@ -4,10 +4,7 @@ require_once __DIR__.'/vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-$request = Request::createFromGlobals();
-
-$input = $request->get('name', 'World');
-
-$response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
-
+$response = new Response();
+$response->setStatusCode(404);
+$response->setContent('Not Found');
 $response->send();
