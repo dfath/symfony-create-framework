eb4cec2a43043cf45346a4c3979ef5eca90e6ac4
dfath <fathrico@gmail.com>
2016-12-16 10:27:15 +0700

    controller resolver

--- framework/src/app.php
+++ framework/src/app.php
@@ -1,6 +1,19 @@
 <?php
 use Symfony\Component\Routing;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class LeapYearController
+{
+    public function indexAction($year)
+    {
+        if (is_leap_year($year)) {
+            return new Response('Yep, this is a leap year!');
+        }
+
+        return new Response('Nope, this is not a leap year.');
+    }
+}
 
 function is_leap_year($year = null) {
     if (null === $year) {
@@ -13,13 +26,6 @@ function is_leap_year($year = null) {
 $routes = new Routing\RouteCollection();
 $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
     'year' => null,
-    '_controller' => function ($request) {
-        if (is_leap_year($request->attributes->get('year'))) {
-            return new Response('Yep, this is a leap year!');
-        }
-
-        return new Response('Nope, this is not a leap year.');
-    }
+    '_controller' => 'LeapYearController::indexAction',
 )));
-
 return $routes;
